634dcdf52e1bff8bcc3b44988fa72061824dac0b
wiily <willyhuang18@gmail.com>
2022-02-19 15:25:21 -0800

    starting the project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -12,6 +10,7 @@ export default function Home() {
       </Head>
 
       <h1>Welcome to the little red book</h1>
+      {/* Header */}
       
     </div>
   )
