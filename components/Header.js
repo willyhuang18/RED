bc89760d64d88ddc6cd67ebe2058a27ed5dbd868
wiily <willyhuang18@gmail.com>
2022-02-19 18:00:29 -0800

    importing icon

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,33 @@
 import Image from "next/image";
+import{
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon,
+} from "@heroicons/react"
 
 function Header(){
     return(
         <div>
-            <h1>I am a little red book</h1>
-            <div className="">
-                <Image src='https://upload.wikimedia.org/wikipedia/commons/c/c1/XiaohongshuLOGO.png'
-                    layout="fill" alt=""
-                />
-            </div>
+        {/* header */}
+            <div className="flex justify-between max-w-6xl">
+                <div className="relative hidden lg:inline-grid h-24 w-24">
+                    <Image src='https://upload.wikimedia.org/wikipedia/commons/c/c1/XiaohongshuLOGO.png'
+                        layout="fill" alt="" objectFit="contain"
+                    />
+                </div>
+                <div className="relative w-10 h-10 lg:hidden flex-shrink-lg cursor-pointer">
+                    <Image src='https://upload.wikimedia.org/wikipedia/commons/c/c1/XiaohongshuLOGO.png'
+                        layout="fill" alt="" objectFit="contain"
+                    />
+                </div>
+                {/* Middle */}
+                <div>
+                <input type="text" placeholder="" />
+                </div>
+            </div> 
         </div>
     )
 }
