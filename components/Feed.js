70e91a084880931294980767d9dc9b2e286dbd28
wiily <willyhuang18@gmail.com>
2022-02-20 21:21:02 -0800

    finish first part of stories

--- components/Feed.js
+++ components/Feed.js
@@ -1,14 +1,17 @@
+import Post from "./Post"
 import Stories from "./Stories"
 
 function Feed(){
     return (
         <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
-        xl:grid-cols-3 xl:max-w-6xl max-auto">
+        xl:grid-cols-3 xl:max-w-6xl mx-auto">
             {/* section */}
             <section className="col-span-2">
             {/* stories */}
             <Stories />
             {/* posts */}
+            <Post />
+            
             </section>
 
             <section>
--- components/Story.js
+++ components/Story.js
@@ -1,9 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 function Story({ img, username }){
     return (
-        <div>
-        <img src={img} alt="" />
-        <p>{username}</p>
+        <div >
+        <img className="h-14 w-14 rounded-full p-[1.5px] 
+        border-red-500 border-2 object-contain cursor-pointer
+        hover:scale-110 transition transform duration-200 ease-out" src={img} alt="" />
+        <p className="text-xs w-14 truncate text-center">{username}</p>
         </div>
     )
 };
