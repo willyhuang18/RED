e70d1eaa492aa028f44e8a887de2a476b57f7fe7
wiily <willyhuang18@gmail.com>
2022-03-19 14:29:25 -0700

    make the minipofile fixed

--- components/Feed.js
+++ components/Feed.js
@@ -18,7 +18,7 @@ function Feed(){
             </section>
 
             <section className="hidden xl:inline-grid md:col-span-1 ">
-                <div>
+                <div className="fixed top-20" >
                 {/* mini profile */}
                 <MiniProfile />
                 {/* suggestions */}
