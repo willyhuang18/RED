74044b2b8a86ed3ee2b7b0bf2bee9bb676f8eedc
wiily <willyhuang18@gmail.com>
2022-03-19 14:32:56 -0700

    adding some styling for the miniprofile

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -5,12 +5,11 @@ function MiniProfile() {
         <img className="rounded-full border p-[2px] w-16 h-16" 
         src="https://upload.wikimedia.org/wikipedia/commons/4/45/Jackson_Wang_at_a_mini_fanmeeting_outside_%22Show%21_Music_Core%22_studios%2C_1_June_2019_02.jpg" 
         alt=""/>
-        <div>
-         <h2>Jackson Wang</h2>
-         <h3>Welcome to the RED</h3>  
+        <div className="flex-1 mx-4">
+            <h2 className="font-bold">Jackson Wang</h2>
+            <h3 className="text-sm text-gray-400">Welcome to RED</h3>  
         </div>
-
-        <button> Sign out</button>
+        <button className="text-blue-400 text-sm font-semibold"> Sign out</button>
     </div>
   )
 }
