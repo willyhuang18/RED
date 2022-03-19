85c3e02017874b27b0235501971992196dd99532
wiily <willyhuang18@gmail.com>
2022-03-19 14:37:50 -0700

    importing the faker data from the suggestion file

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import faker from '@faker-js/faker';
+import { useState, useEffect } from 'react'
 
 function Suggestions() {
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        const suggestions = [...Array(5)].map((_, i) => (
+            {
+                ...faker.helpers.contextualCard(),
+                id: i
+            }
+        ));
+        setSuggestions(suggestions);
+    },[])
   return (
-    <div>Suggestions</div>
+    <div>
+        
+    </div>
   )
 }
 
