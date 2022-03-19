4c2870a80b345a32a04c077d8090c255491d28b0
wiily <willyhuang18@gmail.com>
2022-03-19 14:18:57 -0700

    adding the img for the mini profile

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,11 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 function MiniProfile() {
   return (
     <div className="flex items-center">
         <img className="rounded-full border p-[2px] w-16 h-16" 
-        src="https://upload.wikimedia.org/wikipedia/commons/4/45/Jackson_Wang_at_a_mini_fanmeeting_outside_%22Show%21_Music_Core%22_studios%2C_1_June_2019_02.jpg" alt=""
+        src="https://upload.wikimedia.org/wikipedia/commons/4/45/Jackson_Wang_at_a_mini_fanmeeting_outside_%22Show%21_Music_Core%22_studios%2C_1_June_2019_02.jpg" 
+        alt=""/>
         
-
-        />
     </div>
   )
 }
