64ee826778d60aac5c0820adbfa2d51f7abba14c
wiily <willyhuang18@gmail.com>
2022-03-19 22:08:20 -0700

    making the every suggestion as card

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -6,7 +6,7 @@ function Suggestions() {
     const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
-        const suggestions = [...Array(5)].map((_, i) => (
+        const suggestions = [...Array(10)].map((_, i) => (
             {
                 ...faker.helpers.contextualCard(),
                 id: i
@@ -15,27 +15,28 @@ function Suggestions() {
         setSuggestions(suggestions);
     },[])
   return (
-      <div className='mt-4 ml-10'>
+      <div className='mt-4 ml-10 max-w-[400px]'>
         <div className='flex justify-between  text-sm mb-5'>
             <h3 className='text-sm font-bold text-gray-400'>Suggestions for you</h3>
             <button className='text-gray-500 font-semibold'>See All</button>
         </div>
+        <div className='flex flex-rows p-6 gap-4 rounded shadow-lg 
+        overflow-x-scroll max-w-[1000px] '>
         {suggestions.map((profile) =>(
-            <div 
+            <card  
             key={profile.id}
-            className='flex items-center justify-between mt-3'>
-            <img className="w-10 h-10 rounded-full border p-[2px]"
+            className='w-full rounded-lg border shadow-md flex flex-col p-5 bg-white items-center '>
+            <img className="w-10 h-10 rounded-full border  "
             src={profile.avatar}
             alt='' />
-            <div className='flex-1 ml-4'>
-                <h2 className='font-semibold text-sm'>
+                <h2 className='font-semibold text-sm mt-3'>
                 {profile.username}</h2>
-                <h3 className='text-xs text-gray-400'>
+                <h3 className='text-xs text-gray-400 mt-3 '>
                 Works at {profile.company.name}</h3>
-            </div>
-            <button className='text-blue-400 text-xs font-bold'>Follow</button>
-            </div>
+            <button className='text-blue-400 text-xs font-bold mt-3'>Follow</button>
+            </card>
         ))}
+        </div>
     </div>
   )
 }
