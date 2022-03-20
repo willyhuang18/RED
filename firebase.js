5a40eb3babfb19c67c30df66da3b234dbc98afba
wiily <willyhuang18@gmail.com>
2022-03-20 15:49:30 -0700

    adding firebase package

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLeEfaVM8x1yM6dovbDGnpHCkZ8eGjFyk",
+  authDomain: "red-1-project.firebaseapp.com",
+  projectId: "red-1-project",
+  storageBucket: "red-1-project.appspot.com",
+  messagingSenderId: "500913736030",
+  appId: "1:500913736030:web:6d377a96d5d6f48f3ba7cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
