87e49e6717d236f228dcebeb6ac643762d3df2af
wiily <willyhuang18@gmail.com>
2022-03-21 11:25:46 -0700

    adding user story if the logged in

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import faker from '@faker-js/faker';
 import { useEffect, useState } from "react"
 import Story from './Story';
+import {useSession} from 'next-auth/react'
 
 function Stories(){
     const [suggestions, setSuggestions] = useState([])
+    const { data: session } = useSession();
 
     useEffect(()=>{
         const suggestion = [...Array(20)].map((_, i) =>({
@@ -18,6 +20,13 @@ function Stories(){
         <div className='flex space-x-2 p-6 bg-white mt-8 
         border-gray-200 border rounded-sm overflow-x-scroll
         scrollbar-thin scrollbar-thumb-black '>
+
+        {session && (
+            <Story img={session.user.image}
+                username={session.user.username}
+            />
+        )}
+
         {suggestions.map(profile => (
             <Story 
             key={profile.id} 
