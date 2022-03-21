80da290645ebc5e7d2ca52bd3903352da876a066
wiily <willyhuang18@gmail.com>
2022-03-21 15:01:36 -0700

    implementing the headlessui for the modal styling

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,18 @@
 import { useRecoilState } from "recoil"
 import { modalState } from "../atoms/modalAtom"
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment } from "react";
 
 function Modal() {
     const [open, setOpen] = useRecoilState(modalState)
   return (
-    <div>Modal</div>
+    <Transition.Root show={open} as={Fragment} >
+        <Dialog as="div"
+        className='fixed z-10 inset-= overflow-y-auto'
+        onClose={setOpen}>
+        <h1>Hello</h1>
+        </Dialog>
+    </Transition.Root>
   )
 }
 
