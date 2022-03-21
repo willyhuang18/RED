f62748615fe26b44b9c2a857c8583145f1139698
wiily <willyhuang18@gmail.com>
2022-03-21 15:26:55 -0700

    adding the transition effect for the modal

--- components/Modal.js
+++ components/Modal.js
@@ -10,7 +10,18 @@ function Modal() {
         <Dialog as="div"
         className='fixed z-10 inset-= overflow-y-auto'
         onClose={setOpen}>
-        <h1>Hello</h1>
+        <div className="flex items-end justify-center min-h-[800px] 
+        sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0">
+
+            </Transition.Child>
+        </div>
         </Dialog>
     </Transition.Root>
   )
