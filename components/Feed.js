71630cb8cbf8960e35f25da21d6c3203e877e3ac
wiily <willyhuang18@gmail.com>
2022-03-20 22:15:09 -0700

    implementing next auth authentication

--- components/Feed.js
+++ components/Feed.js
@@ -9,8 +9,8 @@ function Feed(){
     const { data: session } = useSession();
 
     return (
-        <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
-        xl:grid-cols-3 xl:max-w-6xl mx-auto">
+        <main className= {`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
+        xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
             {/* section */}
             <section className="col-span-2">
             {/* stories */}
@@ -18,9 +18,9 @@ function Feed(){
                 <Suggestions />
             {/* posts */}
             <Posts />
-            
             </section>
-
+            
+            {session && (
             <section className="hidden xl:inline-grid md:col-span-1 ">
                 <div className="fixed top-20" >
                 {/* mini profile */}
@@ -28,6 +28,7 @@ function Feed(){
                 {/* suggestions */}
                 </div>
             </section>
+            )}
         </main>
     )
 }
