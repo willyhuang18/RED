c327fecfdf038452cd081bcf1f45b1153f626697
wiily <willyhuang18@gmail.com>
2022-03-21 14:50:58 -0700

    implementing the recoil atoms modal

--- components/Header.js
+++ components/Header.js
@@ -11,9 +11,12 @@ import{
 import { HomeIcon } from "@heroicons/react/solid"
 import { signIn, signOut, useSession } from "next-auth/react";
 import {useRouter} from 'next/router';
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
 
 function Header(){
     const { data: session } = useSession();
+    const [open, setOpen] = useRecoilState(modalState)
     const router = useRouter();
 
     console.log(session);
