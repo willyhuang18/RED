d2fb7945118c7faea631a7f4f83b01b4e46f38f9
wiily <willyhuang18@gmail.com>
2022-03-20 21:39:59 -0700

    styling the sign in button and the logo

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,13 +5,20 @@ function signIn({providers}) {
   return (
     <>
     <Header />
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14">
+    <img className="w-80" 
+    src='https://viceclicks.com/wp-content/uploads/2020/04/pasted-image-0-3.png' 
+    alt=""/>
+      <div className="mt-40">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => SignIntoProvider(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
     </>
   );
 }
