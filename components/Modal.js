7b84670f1d3313f69cb7ecf8010180a5c8b206fc
wiily <willyhuang18@gmail.com>
2022-03-21 21:42:01 -0700

    finalizing the upload image to the database

--- components/Modal.js
+++ components/Modal.js
@@ -44,7 +44,9 @@ function Modal() {
                 image: downloadURL
             })
         });
-        
+        setOpen(false);
+        setLoading(false);
+        setSelectedFile(null);
     }
 
     const addImageToPost =(e) => {
@@ -139,12 +141,14 @@ function Modal() {
                             </div>
                             <div className="mt-5 sm:mt-6">
                                 <button type="button"
+                                disabled= {!selectedFile}
                                 className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm
                                 px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
                                 focus:  focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed
                                 hover:disabled:bg-gray-300"
+                                onClick={uploadPost}
                                 >
-                                Upload Post
+                                {loading ? "Uploading..." : "Upload Post"}
                                 </button>
                             </div>
                         </div>
