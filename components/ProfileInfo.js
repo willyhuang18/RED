5b1379f1741d878ff5011714e490572910313078
wiily <willyhuang18@gmail.com>
2022-03-23 19:27:56 -0700

    move the profile right a little

--- components/ProfileInfo.js
+++ components/ProfileInfo.js
@@ -1,7 +1,7 @@
 function ProfileInfo() {
   return (
       <div className='mt-4  max-w-[1000px]'>
-        <div className='flex flex-col p-6 gap-4 text-sm mb-5'>
+        <div className='flex flex-col pl-10 gap-4 text-sm mb-5'>
             <h3 className='text-lg font-bold text-black'>DashBoard</h3>
             <h3 className='text-lg font-bold text-black'>Profile</h3>
             <h3 className='text-lg font-bold text-black'>Favorite</h3>
