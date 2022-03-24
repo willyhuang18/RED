990f2ef860b06d1ece68a0b939876ea211e271cf
wiily <willyhuang18@gmail.com>
2022-03-23 19:29:57 -0700

    making the word smaller

--- components/ProfileInfo.js
+++ components/ProfileInfo.js
@@ -2,10 +2,10 @@ function ProfileInfo() {
   return (
       <div className='mt-4  max-w-[1000px]'>
         <div className='flex flex-col pl-10 gap-4 text-sm mb-5'>
-            <h3 className='text-lg font-bold text-black'>DashBoard</h3>
-            <h3 className='text-lg font-bold text-black'>Profile</h3>
-            <h3 className='text-lg font-bold text-black'>Favorite</h3>
-            <h3 className='text-lg font-bold text-black'>Setting</h3>
+            <h3 className='text-md font-bold text-black'>DashBoard</h3>
+            <h3 className='text-md font-bold text-black'>Profile</h3>
+            <h3 className='text-md font-bold text-black'>Favorite</h3>
+            <h3 className='text-md font-bold text-black'>Setting</h3>
         </div>
     </div>
   )
